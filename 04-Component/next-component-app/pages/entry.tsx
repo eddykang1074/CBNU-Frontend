4a88a10591cae18d18b5c77c650b13f418012e5d
eddy <ceo@msoftware.co.kr>
2024-08-12 12:00:27 +0900

    전화번호 패딩 수정

--- 04-Component/next-component-app/pages/entry.tsx
+++ 04-Component/next-component-app/pages/entry.tsx
@@ -114,7 +114,7 @@ const Entry = () => {
                 name="telephone"
                 type="tel"
                 autoComplete="tel"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 pl-32 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
