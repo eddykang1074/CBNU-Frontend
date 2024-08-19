7f773155f8a49dae83d02908262f33dbeac9a965
Eddy <eddy@msoftware.co.kr>
2024-08-19 12:03:48 +0900

    회원가입 처리

--- Final/next-platform-app/pages/regist.tsx
+++ Final/next-platform-app/pages/regist.tsx
@@ -1,6 +1,62 @@
-import { GetServerSideProps } from "next";
+//신규회원가입 페이지 컴포넌트
+//호출주소 : http://localhost:3003/regist
+
+//화면상에 데이터 관리를 위한 useState 훅 참조하기
+import { useState } from 'react';
+
+//신규회원 가입정보 인터페이스 타입 참조하기
+import { IEntryMember } from '@/interfaces/member';
 
 const Regist = () => {
+  //신규 회원가입 정보 상태 데이터 정의 및 값 초기화처리
+  //useState(초기값설정) 함수는 [변수,변수값변경 세터함수] 배열을 반환한다.
+  const [member, setMember] = useState<IEntryMember>({
+    email: '',
+    password: '',
+    name: '',
+  });
+
+  //사용자 입력요소의 값이 변경될떄마다 데이터소스와 동기화 처리해주는 이벤트처리함수
+  const memberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //...member란 현재 member상수의 복사본 객체를 생성한다는 의미
+    //e.target은 Change 이벤트가 발생한 UI요소를 말한다.
+    //[e.target.name = 이벤트가발생한 요소의 name속성값]:e.target.name(이벤트가 발생한 요소의 현재 입력 value값)
+    setMember({ ...member, [e.target.name]: e.target.value });
+  };
+
+  //회원가입 버튼 클릭시 신규 회원정보 백엔드 처리함수
+  const registSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    // e.preventDefault();Form Submit이벤트가 호출되면 어떤식으로든 화면을 리프레시하는 기능이 작동되므로 해당 form submit이벤트를 취소하여
+    //화면이 리프레시되는 현상을 방지한다.
+    e.preventDefault();
+
+    //백엔드 RESTful API 중 신규회원가입 API를 fetch() ajax호출기능을 통해 가입정보를 백엔드로 전달한다.
+    //예시1) ES2015 자바스크립트 기본 AJAX 통신 내장 라이브러리인 fetch를 이용해 백엔드와 통신하기
+    //await fecth("백엔드 API호출주소",호출옵션);
+    //fetch()함수를 통해 데이터를 백엔드로 전달할때는 반드시 json문자열 형태로 전달합니다.
+    //JSON.stringify(json데이터);json데이터를 json문자열로 변경해주는 내장함수
+
+    try {
+      //Step1) fetch()함수 호출하기
+      const response = await fetch('http://localhost:5000/api/member/entry', {
+        method: 'POST',
+        body: JSON.stringify(member),
+      });
+
+      //Step2: fetch()함수 호출결과 백엔드 반환 실제 데이터 추출하기
+      const result = await response.json();
+
+      if (result.code == 200) {
+        console.log('백엔드에서 제공한 json데이터 확인:', result);
+        //정상적으로 회원가입된 경우 로그인 페이지 컴포넌트로 이동처리
+      } else {
+        console.log('백엔드 서버 에러발생:', result.msg);
+      }
+    } catch (err) {
+      console.error('백엔드 REST API호출중에 에러가 발생했습니다.');
+    }
+  };
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -16,7 +72,9 @@ const Regist = () => {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          {/* 신규 회원가입  폼영역 */}
+          <form className="space-y-6" onSubmit={registSubmit}>
+            {/* 메일주소 입력요소 영역 */}
             <div>
               <label
                 htmlFor="email"
@@ -29,6 +87,8 @@ const Regist = () => {
                   id="email"
                   name="email"
                   type="email"
+                  value={member.email}
+                  onChange={memberChange}
                   autoComplete="email"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -36,6 +96,7 @@ const Regist = () => {
               </div>
             </div>
 
+            {/* 사용자 암호 입력요소 영역 */}
             <div>
               <div className="flex items-center justify-between">
                 <label
@@ -50,6 +111,8 @@ const Regist = () => {
                   id="password"
                   name="password"
                   type="password"
+                  value={member.password}
+                  onChange={memberChange}
                   autoComplete="current-password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -57,20 +120,23 @@ const Regist = () => {
               </div>
             </div>
 
+            {/* 사용자 이름 입력요소 영역 */}
             <div>
               <div className="flex items-center justify-between">
                 <label
-                  htmlFor="password_cofirm"
+                  htmlFor="name"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Password Confirm
+                  Name
                 </label>
               </div>
               <div className="mt-2">
                 <input
-                  id="password_cofirm"
-                  name="password_cofirm"
-                  type="password"
+                  id="name"
+                  name="name"
+                  type="text"
+                  value={member.name}
+                  onChange={memberChange}
                   autoComplete="current-password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -78,6 +144,7 @@ const Regist = () => {
               </div>
             </div>
 
+            {/* 회원가입 버튼 표시영역 */}
             <div>
               <button
                 type="submit"
@@ -89,7 +156,7 @@ const Regist = () => {
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Already a member?{" "}
+            Already a member?{' '}
             <a
               href="#"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
